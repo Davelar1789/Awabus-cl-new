c6a56b47b65cc5cfd821ec2c8614c9eaae3a21ed
Davelar1789 <dnalaryea003@st.ug.edu.gh>
2026-09-25 19:12:04 +0000

    add another

--- admin/src/pages/students/AddStudent.jsx
+++ admin/src/pages/students/AddStudent.jsx
@@ -218,7 +218,7 @@ export default function AddStudent() {
                 clearDraft();
               }}
             >
-              Add Student
+              Add Another Student
             </Button>
           </div>
         </Card>
